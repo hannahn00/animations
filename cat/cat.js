6052f11db91c989a9a5c0bc7d12dda97cf3f0af6
Hannah Nguyen <hannahng004@gmail.com>
2023-12-24 21:04:28 -0500

    stylized cat

---
+++ cat/cat.js
@@ -0,0 +1,98 @@
+//setup
+function setup() {
+
+    createCanvas(800, 800);
+    frameRate(16);
+    noStroke();
+
+}
+
+//main draw function
+function draw() {
+
+    background(75, 82, 126);
+
+    //cat head
+    fill(17, 32, 49);
+    ellipse(400, 450, 400, 300)
+
+
+    //cat ears
+    fill(17, 32, 49);
+    triangle(200, 197, 206, 416, 315, 315);
+    triangle(600, 197, 593, 416, 481, 315);
+
+
+    //cat nose
+    fill(124, 129, 173);
+    triangle(370, 480, 430, 480, 400, 500);
+
+
+    //delete later, x and y coordinates
+    fill(255);
+    text("x:" + mouseX, 0, 30, 30);
+    text("y:" + mouseY, 0, 60, 30);
+
+
+    //text
+    fill(240, 232, 199);
+    textSize(15);
+    text("dont wake Mr. Whiskers", 320, 200);
+    
+    
+    //if 'o' key is pressed, open cat eyes and mouth
+    if ((keyIsPressed == true) && (key == 'o')) {
+        
+
+        //open eyes
+        fill(242, 247, 161);
+        ellipse(300, 370, 50, 50);
+        ellipse(500, 370, 50, 50);
+
+
+        //pupils
+        fill(39, 55, 77);
+        ellipse(300, 370, 20, 50);
+        ellipse(500, 370, 20, 50);
+
+
+        //mouth
+        fill(102, 37, 73);
+        ellipse(400, 480, 120, 170);
+
+        
+        //aftermath nose
+        fill(124, 129, 173);
+        triangle(370, 375, 430, 375, 400, 395);
+    
+
+    }
+
+
+      //default to closed eyes  
+      else { 
+        
+        push();
+
+        stroke(4, 3, 18); // Set stroke color to white
+        strokeWeight(5);
+        noFill();    // Disable fill
+        
+        //left eye
+        arc(320, 420, 50, 20, 0, PI / 1.0);
+
+        //right eye
+        arc(480, 420, 50, 20, 0, PI / 1.0); 
+        
+        pop();
+    }
+
+    push();
+    strokeWeight(8)
+    stroke(0);
+    fill(0);
+    //circle(mouseX, mouseY, 15);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    pop();
+
+}
