84a26d12d3fa9718d3f3203c45c321e811778e0e
Hannah Nguyen <hannahng004@gmail.com>
2023-12-25 15:27:35 -0500

    DONT W CAT

--- cat/cat.js
+++ cat/cat.js
@@ -1,3 +1,11 @@
+//cat sound
+let mySound;
+function preload() {
+  soundFormats('mp3', 'ogg');
+  mySound = loadSound('catscream.mp3');
+}
+
+
 //setup
 let points = [];
 
@@ -6,6 +14,12 @@ function setup() {
     createCanvas(800, 800);
     frameRate(16);
     noStroke();
+
+    // Play the sound once when the sketch is set up
+    if (mySound.isLoaded()) {
+        mySound.play();
+    }
+
 
 }
 
@@ -31,63 +45,116 @@ function draw() {
 
 
     //delete later, x and y coordinates
-    fill(255);
+    fill(75, 82, 126, 90);
     text("x:" + mouseX, 0, 30, 30);
     text("y:" + mouseY, 0, 60, 30);
 
 
     //text
-    fill(240, 232, 199);
+    fill(235, 232, 199);
     textSize(15);
-    text("dont wake Mr. Whiskers", 320, 200);
+    text("click Mr. Whiskers' mouth", 315, 200);
     
     
-    //if 'o' key is pressed, open cat eyes and mouth
-    if ((keyIsPressed == true) && (key == 'o')) {
+    //left eye
+    if (dist(mouseX, mouseY, 320, 430) < 30) {
         
-
-        //open eyes
+        //sclera
         fill(242, 247, 161);
-        ellipse(300, 370, 50, 50);
-        ellipse(500, 370, 50, 50);
-
-
-        //pupils
-        fill(39, 55, 77);
-        ellipse(300, 370, 20, 50);
-        ellipse(500, 370, 20, 50);
-
-
-        //mouth
-        fill(102, 37, 73);
-        ellipse(400, 480, 120, 170);
-
+        ellipse(320, 420, 50, 50);
         
-        //aftermath nose
-        fill(124, 129, 173);
-        triangle(370, 375, 430, 375, 400, 395);
-    
+
+        //pupil
+        fill(39, 55, 77);
+        ellipse(320, 420, 20, 50);
+
+        //closed right eye
+        push();
+
+        stroke(4, 3, 18);
+        strokeWeight(5);
+        noFill(); 
+
+        //right eye
+        arc(480, 420, 50, 20, 0, PI / 1.0); 
+        pop();
 
     }
 
-
-      //default to closed eyes  
-      else { 
+    //right eye
+    else if (dist(mouseX, mouseY, 480, 430) < 30) {
         
+        //sclera 
+        fill(242, 247, 161);
+        ellipse(480, 420, 50, 50);
+        
+
+        //pupil
+        fill(39, 55, 77);
+        ellipse(480, 420, 20, 50);
+
+        //closed left eye
+        push();
+
+        stroke(4, 3, 18); 
+        strokeWeight(5);
+        noFill();    
+
+        //left eye
+        arc(320, 420, 50, 20, 0, PI / 1.0);
+        
+        pop();
+
+    }
+    
+
+    //around nose -> cat is awake
+    else if (dist(mouseX, mouseY, 400, 488) < 10) {
+        
+        //sclera
+        fill(242, 247, 161);
+        ellipse(300, 370, 50, 50);
+        ellipse(500, 370, 50, 50);
+        
+        
+        //pupil
+        fill(39, 55, 77);
+        ellipse(300, 370, 20, 50);
+        ellipse(500, 370, 20, 50);
+        
+        
+        //aftermath nose 
+        fill(124, 129, 173);
+        triangle(370, 375, 430, 375, 400, 395);
+        
+
+        //open mouth
+        fill(102, 37, 73);
+        ellipse(400, 480, 120, 170);
+
+        //sound
+        mySound.play();
+        
+    }
+
+
+    //default
+    else {
         push();
 
         stroke(4, 3, 18); // Set stroke color to white
         strokeWeight(5);
         noFill();    // Disable fill
-        
+            
         //left eye
         arc(320, 420, 50, 20, 0, PI / 1.0);
 
         //right eye
         arc(480, 420, 50, 20, 0, PI / 1.0); 
-        
+            
         pop();
-    }
+
+    }    
 
     // Draw the curve
     push();
@@ -109,6 +176,5 @@ function draw() {
     if (points.length > 5) {
         points.shift();
     }
-
-
+    
 }
