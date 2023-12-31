972e7b16ceee954d4a4ff20b66501c7e31c4c635
Hannah Nguyen <hannahng004@gmail.com>
2023-12-31 14:55:32 -0500

    getting somewhere...

--- tree/tree.js
+++ tree/tree.js
@@ -1,115 +1,68 @@
 let rules = {
-    A: "[&FL!A]/////’[&FL!A]///////’[&FL!A]",
-    F: "S ///// F",
-    S: "F L",
-    L: "[’’’∧∧{-f+f+f-|-f+f+f}]"
+    //X: "F[+X][-X]",
+    X: "F[+X-X][X+X][-X+X][-X-X]",
+    F: "FF",
   }
   
-  let len = 7;
+  let len = 3;
   let ang;
-  //let diameter = 10; // Initial diameter
-  //let colorIndex = 0; // Initial color index
   
   
   let drawRules;
   
-  let word = "A";
+  let word = "X";
   
   function setup() {
-    createCanvas(800, 800);
-    ang = radians(22.5); 
+    createCanvas(400, 400);
+    ang = (PI/180 * 22); 
     drawRules = {
       "F": () => {
         stroke(100, 50, 0);
         line(0, 0, 0, -len); 
         translate(0, -len); 
-        //push();
-        //translate(0, newLen / 2, 0); 
-        //ellipse(0, 0, 1 * len, 5 * len);
-        //pop();
       },
-
-      "A": () => {
-        // Draw circle at current location
-        noStroke();
-        fill("#E5CEDC");
-        circle(0, 0, len*2);
-      },  
-      "L": () => {
-        // Draw circle at current location
-        noStroke();
-        fill("#FCA17D");
-        circle(0, 0, len*2);
-      },
-  
-      
       /*
       "f": () => {
-        translate(0, -len, 0);
+        stroke(100, 50, 0);
+        line(0, 0, 0, -len); 
+        translate(0, -len); 
       },
-      */
-      
+*/
+      "B": () => {
+        noStroke();
+        fill("#E5CEDC");
+        circle(0, 0, 20);
+      },  
+      "A": () => {
+        noStroke();
+        fill("#FCA17D");
+        circle(0, 0, 20);
+      },
+        
       "+": () => {
         rotate(-ang); 
       },
-      
+            
       "-": () => {
         rotate(ang);
       },
+
   
       "[": () => {
         push();
       },
         
       "]": () => {
-        //noStroke();
-        //fill(0, 200, 0);
-        //ellipse(0, 0, 2 * len, 5 * len);
         pop();
       }
     }
     noLoop();
-
-      
-    /*
-      "&": () => {
-        rotate(-ang);
-      },
-    
-      "∧": () => {
-        rotate(ang); 
-      },
-    
-      "\\": () => {
-        rotate(-ang);
-      },
-  
-      "/": () => {
-        rotate(ang);
-      },
-    
-      "|": () => {
-        rotate(PI); 
-      },
-      
-      "!": () => {
-        diameter--;
-        diameter = max(diameter, 1); 
-      },
-  
-      "": () => {
-        colorIndex++;
-      }
-    }
-  */
   }
   
   function draw() {
     background(220);
-    
-    //rotateX(PI/2); // Adjust rotation for the 3D coordinate system
     push();
-    translate(200, 200);
+    translate(200, 400);
     for(let i = 0; i < word.length; i ++) {
       let c = word[i];
       if(c in drawRules) {
